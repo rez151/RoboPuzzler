ec9ffdb74686b592485128575cee6c97315552db
Aaron Bucher <aaron.bucher@hs-furtwangen.de>
2019-05-15 23:26:14 +0200

    Bild spricht auf die Slider an, Delay abgeändert

--- ObjectDetection/Gui.py
+++ ObjectDetection/Gui.py
@@ -1,14 +1,15 @@
+
+import tkinter
 from tkinter import *
 from tkinter import filedialog
-import PIL.Image, PIL.ImageTk
-import time
 
+import PIL.Image
+import PIL.ImageTk
 import cv2
 
 
 class GUI:
     def mainLoop(self):
-
         def gets():
             print("Threshold " + str(scalethresh.get()))
             print("Erode " + str(scaleerode.get()))
@@ -56,24 +57,23 @@ class GUI:
         lbl_dilate = Label(configframe, text="Dilate: ")
         lbl_dilate.grid(row=2, column=0)
 
-        #dilateVar = scaledilate.get()
         scaledilate = Scale(configframe,  from_=0, to=15, orient=HORIZONTAL)
         dilateVar = scaledilate.get()
         scaledilate.grid(row=2, column=1)
 
-        button = Button(configframe, text="Dilate", command=gets)
-        button.grid(row=5, column=1)
-
-        label_dil = Label(configframe, text="Label")
-        label_dil.grid(row=6, column=1)
-
-
         lbl_model = Label(configframe, text="Model: ")
         lbl_model.grid(row=3, column=0)
+
+
+        button = tkinter.Button(configframe, text="Text", command=gets)
+        button.config(bg="white", fg="black")
+        button.grid(row=5, column=1)
 
         modelPath = StringVar()
         entrymodel = Entry(configframe)
         entrymodel.grid(row=3, column=1)
+
+
 
         def openFile():
             file = filedialog.askopenfile(initialdir="model", title="Select model",
@@ -82,22 +82,23 @@ class GUI:
             entrymodel.insert(0,modelPath)
             pass
 
-        buttonfileopener = Button(configframe,text="...", command=openFile,fg="black")
+        buttonfileopener = Button(configframe,text="...", fg="black", command=openFile)
         buttonfileopener.grid(row=3, column=2)
 
         delay = 1
-        thresh, width, height = getVideo(threshVar, erodeVar, dilateVar)
+        thresh, width, height = getVideo(threshVar=scalethresh.get(), erodeVar=scaleerode.get(), dilateVar=scaledilate.get())
         can = Canvas(imageframe, width=width, height=height, bg="gray")
         can.pack()
 
         def update():
-            thresh, width, height = getVideo(threshVar, erodeVar, dilateVar)
+            thresh, width, height = getVideo(threshVar=scalethresh.get(), erodeVar=scaleerode.get(), dilateVar=scaledilate.get())
             photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(thresh))
             can.create_image(0, 0, image=photo, anchor=NW)
             can.update()
             root.after(delay, update)
 
         update()
+
 
         root.mainloop()
 
@@ -124,5 +125,7 @@ def getVideo(threshVar,erodeVar,dilateVar):
 
         return thresh,width,height
 
+
 if __name__ == '__main__':
+
     GUI().mainLoop()
