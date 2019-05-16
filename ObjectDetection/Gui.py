f82acbcf3af15cc00ee60975c26a29d358418b28
Niels Schneider <niels.schneider@hs-furtwangen.de>
2019-05-16 12:11:50 +0200

    gui update

--- ObjectDetection/Gui.py
+++ ObjectDetection/Gui.py
@@ -3,18 +3,19 @@ import tkinter
 from tkinter import *
 from tkinter import filedialog
 
-import PIL.Image
-import PIL.ImageTk
+from PIL import Image
+from PIL import ImageTk
 import cv2
 
 
 class GUI:
-    def mainLoop(self):
-        def gets():
-            print("Threshold " + str(scalethresh.get()))
-            print("Erode " + str(scaleerode.get()))
-            print("Dilate " + str(scaledilate.get()))
+    # Standard values
+    threshVar = 100
+    erodeVar = 8
+    dilateVar = 2
 
+
+    def mainLoop(self):
         root = Tk()
         root.title("Rob der Puzzler")
 
@@ -39,92 +40,98 @@ class GUI:
         menubar.add_cascade(label="Model", menu=filemenu)
         filemenu.add_command(label="Open", command=donothing)
 
+        self.configArea(configframe)
 
-        lbl_thresh  = Label(configframe,text="Threshold: ")
+        self.modelArea(configframe)
+
+        self.videoPlayer(imageframe)
+
+        root.mainloop()
+
+
+    def videoPlayer(self,imageframe):
+        can = Canvas(imageframe, width=600,height=400)
+        can.create_image(0, 0, anchor=NW)
+        can.pack()
+        self.update(imageframe, can)
+
+    def update(self, root, can, delay=1):
+        thresh, width, height = self.getVideo()
+        image = Image.fromarray(thresh)
+        image = ImageTk.PhotoImage(image)
+        can.create_image(0, 0, image=image, anchor=NW)
+        can.update()
+        root.after(delay, self.update(root,can))
+
+
+
+    def configArea(self,configframe):
+        lbl_thresh = Label(configframe, text="Threshold: ")
         lbl_thresh.grid(row=0, column=0)
 
-        scalethresh = Scale(configframe, from_=0, to=255, orient=HORIZONTAL)
-        threshVar = scalethresh.get()
-        scalethresh.grid(row=0, column=1)
+        self.scalethresh = Scale(configframe, from_=0, to=255, orient=HORIZONTAL)
+        self.scalethresh.set(self.threshVar)
+        threshVar = self.scalethresh.get()
+
+        self.scalethresh.grid(row=0, column=1)
 
         lbl_erode = Label(configframe, text="Erode: ")
         lbl_erode.grid(row=1, column=0)
 
-        scaleerode = Scale(configframe, from_=0, to=15, orient=HORIZONTAL)
-        erodeVar = scaleerode.get()
-        scaleerode.grid(row=1, column=1)
+        self.scaleerode = Scale(configframe, from_=0, to=15, orient=HORIZONTAL)
+        self.scaleerode.set(self.erodeVar)
+        erodeVar = self.scaleerode.get()
+        self.scaleerode.grid(row=1, column=1)
 
         lbl_dilate = Label(configframe, text="Dilate: ")
         lbl_dilate.grid(row=2, column=0)
 
-        scaledilate = Scale(configframe,  from_=0, to=15, orient=HORIZONTAL)
-        dilateVar = scaledilate.get()
-        scaledilate.grid(row=2, column=1)
+        self.scaledilate = Scale(configframe, from_=0, to=15, orient=HORIZONTAL)
+        self.scaledilate.set(self.dilateVar)
+        dilateVar = self.scaledilate.get()
+        self.scaledilate.grid(row=2, column=1)
 
+    def modelArea(self,configframe):
         lbl_model = Label(configframe, text="Model: ")
         lbl_model.grid(row=3, column=0)
-
-
-        button = tkinter.Button(configframe, text="Text", command=gets)
-        button.config(bg="white", fg="black")
-        button.grid(row=5, column=1)
 
         modelPath = StringVar()
         entrymodel = Entry(configframe)
         entrymodel.grid(row=3, column=1)
 
-
-
-        def openFile():
-            file = filedialog.askopenfile(initialdir="model", title="Select model",
-                                          filetypes=(("Model files", "*.h5"), ("All files", "*.*")))
-            modelPath = file.name
-            entrymodel.insert(0,modelPath)
-            pass
-
-        buttonfileopener = Button(configframe,text="...", fg="black", command=openFile)
+        buttonfileopener = Button(configframe, text="...", fg="black", command=lambda :self.openFile(entrymodel))
         buttonfileopener.grid(row=3, column=2)
 
-        delay = 1
-        thresh, width, height = getVideo(threshVar=scalethresh.get(), erodeVar=scaleerode.get(), dilateVar=scaledilate.get())
-        can = Canvas(imageframe, width=width, height=height, bg="gray")
-        photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(thresh))
-        can.create_image(0, 0, image=photo, anchor=NW)
-        can.pack()
 
-        def update():
-            thresh, width, height = getVideo(threshVar=scalethresh.get(), erodeVar=scaleerode.get(), dilateVar=scaledilate.get())
-            photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(thresh))
-            can.create_image(0, 0, image=photo, anchor=NW)
-            can.update()
-            root.after(delay, update)
+    def openFile(self,entrymodel):
+        file = filedialog.askopenfile(initialdir="model", title="Select model",
+                                      filetypes=(("Model files", "*.h5"), ("All files", "*.*")))
+        modelPath = file.name
+        entrymodel.insert(0, modelPath)
+        pass
 
-        update()
 
-        root.mainloop()
+    def getVideo(self):
+        frame = cv2.VideoCapture(0)
+        if not frame.isOpened():
+            raise ValueError("Unable to open video source", 1)
+
+        width = frame.get(cv2.CAP_PROP_FRAME_WIDTH)
+        height = frame.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+        if frame.isOpened():
+            _, image = frame.read()
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            gray = cv2.GaussianBlur(gray, (17, 17), 0)
+            thresh = cv2.threshold(gray, self.threshVar, 255, cv2.THRESH_BINARY)[1]
+            thresh = cv2.erode(thresh, None, iterations=self.erodeVar)
+            thresh = cv2.dilate(thresh, None, iterations=self.dilateVar)
+
+            return thresh,width,height
+
 
 def donothing():
     print("placeholder")
-
-
-def getVideo(threshVar,erodeVar,dilateVar):
-    frame = cv2.VideoCapture(0)
-    if not frame.isOpened():
-        raise ValueError("Unable to open video source", 1)
-
-    width = frame.get(cv2.CAP_PROP_FRAME_WIDTH)
-    height = frame.get(cv2.CAP_PROP_FRAME_HEIGHT)
-
-    if frame.isOpened():
-        _, image = frame.read()
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        gray = cv2.GaussianBlur(gray, (17, 17), 0)
-        thresh = cv2.threshold(gray, threshVar, 255, cv2.THRESH_BINARY)[1]
-        thresh = cv2.erode(thresh, None, iterations=erodeVar)
-        thresh = cv2.dilate(thresh, None, iterations=dilateVar)
-
-        return thresh,width,height
-
 
 if __name__ == '__main__':
 
