9dcd3fec758bab6d17381d17569d0cdd9b8be30a
Niels Schneider <niels.schneider@hs-furtwangen.de>
2019-05-17 14:44:33 +0200

    Gui Live Working with all sliders working, show keras history with plt

--- ObjectDetection/Gui.py
+++ ObjectDetection/Gui.py
@@ -1,147 +1,57 @@
-import tkinter
 from tkinter import *
-from tkinter import filedialog
-import threading
-
-import PIL
-from PIL import Image
-from PIL import ImageTk
 import cv2
+from PIL import Image, ImageTk
 
 
-class GUI:
-    # Standard values
-    threshVar = 100
-    erodeVar = 8
-    dilateVar = 2
-    cap = cv2.VideoCapture(0)
+width, height = 800, 600
+cap = cv2.VideoCapture(0)
 
-    def mainLoop(self):
-        root = Tk()
-        root.title("Rob der Puzzler")
+root = Tk()
 
-        frame = Frame(root,width=800,height=600)
-        frame.pack()
+frame = Frame(root,width=800,height=600)
+frame.pack()
 
-        configframe = Frame(frame)
-        configframe.pack(side=LEFT)
+configframe = Frame(frame)
+configframe.pack(side=LEFT)
 
-        imageframe = Frame(frame)
-        imageframe.pack(side=RIGHT)
+lbl_thresh = Label(configframe, text="Threshold: ")
+lbl_thresh.grid(row=0, column=0)
 
-        menubar = Menu(root)
-        root.config(menu=menubar)
+scalethresh = Scale(configframe, from_=0, to=255, orient=HORIZONTAL)
+scalethresh.grid(row=0, column=1)
 
+lbl_erode = Label(configframe, text="Erode: ")
+lbl_erode.grid(row=1, column=0)
 
-        options = Menu(menubar, tearoff=0)
-        menubar.add_cascade(label="Options", menu=options)
-        options.add_command(label="Exit", command=root.quit)
+scaleerode = Scale(configframe, from_=0, to=15, orient=HORIZONTAL)
+scaleerode.grid(row=1, column=1)
 
-        filemenu = Menu(menubar, tearoff=0)
-        menubar.add_cascade(label="Model", menu=filemenu)
-        filemenu.add_command(label="Open", command=donothing)
+lbl_dilate = Label(configframe, text="Dilate: ")
+lbl_dilate.grid(row=2, column=0)
 
-        self.configArea(configframe)
-
-        self.modelArea(configframe)
-
-        self.videoPlayer(imageframe)
-
-        root.mainloop()
+scaledilate = Scale(configframe, from_=0, to=15, orient=HORIZONTAL)
+scaledilate.grid(row=2, column=1)
 
 
+imageframe = Frame(frame)
+imageframe.pack(side=RIGHT)
 
+lmain = Label(imageframe)
+lmain.pack()
 
+def show_frame():
+    _, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (17, 17), 0)
+    thresh = cv2.threshold(gray,scalethresh.get(), 255, cv2.THRESH_BINARY)[1]
+    thresh = cv2.erode(thresh, None, iterations=scaleerode.get())
+    thresh = cv2.dilate(thresh, None, iterations=scaledilate.get())
+    img = Image.fromarray(thresh)
+    imgtk = ImageTk.PhotoImage(image=img)
+    lmain.imgtk = imgtk
+    lmain.configure(image=imgtk)
+    lmain.after(10, show_frame)
 
-
-    def configArea(self,configframe):
-        lbl_thresh = Label(configframe, text="Threshold: ")
-        lbl_thresh.grid(row=0, column=0)
-
-        self.scalethresh = Scale(configframe, from_=0, to=255, orient=HORIZONTAL)
-        self.scalethresh.set(self.threshVar)
-        threshVar = self.scalethresh.get()
-
-        self.scalethresh.grid(row=0, column=1)
-
-        lbl_erode = Label(configframe, text="Erode: ")
-        lbl_erode.grid(row=1, column=0)
-
-        self.scaleerode = Scale(configframe, from_=0, to=15, orient=HORIZONTAL)
-        self.scaleerode.set(self.erodeVar)
-        erodeVar = self.scaleerode.get()
-        self.scaleerode.grid(row=1, column=1)
-
-        lbl_dilate = Label(configframe, text="Dilate: ")
-        lbl_dilate.grid(row=2, column=0)
-
-        self.scaledilate = Scale(configframe, from_=0, to=15, orient=HORIZONTAL)
-        self.scaledilate.set(self.dilateVar)
-        dilateVar = self.scaledilate.get()
-        self.scaledilate.grid(row=2, column=1)
-
-    def modelArea(self,configframe):
-        lbl_model = Label(configframe, text="Model: ")
-        lbl_model.grid(row=3, column=0)
-
-        modelPath = StringVar()
-        entrymodel = Entry(configframe)
-        entrymodel.grid(row=3, column=1)
-
-        buttonfileopener = Button(configframe, text="...", fg="black", command=lambda :self.openFile(entrymodel))
-        buttonfileopener.grid(row=3, column=2)
-
-
-    def openFile(self,entrymodel):
-        file = filedialog.askopenfile(initialdir="model", title="Select model",
-                                      filetypes=(("Model files", "*.h5"), ("All files", "*.*")))
-        modelPath = file.name
-        entrymodel.insert(0, modelPath)
-        pass
-
-
-    def getVideo(self):
-        frame = cv2.VideoCapture(0)
-        if not frame.isOpened():
-            raise ValueError("Unable to open video source", 1)
-
-        width = frame.get(cv2.CAP_PROP_FRAME_WIDTH)
-        height = frame.get(cv2.CAP_PROP_FRAME_HEIGHT)
-
-        if frame.isOpened():
-            _, image = frame.read()
-            gray = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            gray = cv2.GaussianBlur(gray, (17, 17), 0)
-            thresh = cv2.threshold(gray, self.threshVar, 255, cv2.THRESH_BINARY)[1]
-            thresh = cv2.erode(thresh, None, iterations=self.erodeVar)
-            thresh = cv2.dilate(thresh, None, iterations=self.dilateVar)
-
-            return thresh,width,height
-
-    def videoPlayer(self,imageframe):
-        lbl = Label(imageframe)
-        lbl.pack()
-        self.update(imageframe, lbl)
-
-    def update(self,lbl, delay=10):
-        _, frame = self.cap.read()
-        frame = cv2.flip(frame, 1)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        gray = cv2.GaussianBlur(gray, (17, 17), 0)
-        thresh = cv2.threshold(gray, self.threshVar, 255, cv2.THRESH_BINARY)[1]
-        thresh = cv2.erode(thresh, None, iterations=self.erodeVar)
-        thresh = cv2.dilate(thresh, None, iterations=self.dilateVar)
-        img = PIL.Image.fromarray(thresh)
-        imgtk = ImageTk.PhotoImage(image=img)
-        lbl.imgtk =imgtk
-        lbl.config(image=imgtk)
-        lbl.after(delay, self.update())
-
-
-
-def donothing():
-    print("placeholder")
-
-if __name__ == '__main__':
-
-    GUI().mainLoop()
+show_frame()
+root.mainloop()
--- ObjectDetection/TrainModel.py
+++ ObjectDetection/TrainModel.py
@@ -1,10 +1,10 @@
 from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D
-from keras.layers import Activation, Dropout, Flatten, Dense
+from keras.layers import Conv2D, MaxPooling2D, Activation, Dropout, Flatten, Dense
 from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+
 from keras import backend as K
-from time import time
-import keras
+import matplotlib.pyplot as plt
 
 from ObjectDetection.TensorBoardWrapper import TensorBoardWrapper
 
@@ -20,7 +20,7 @@ class TrainModel:
                 image_hight = 224
                 batch_size = 10
 
-                model = keras.models.Sequential()
+                model = Sequential()
                 model.add(Conv2D(32, (3, 3), input_shape=(image_width, image_hight, 3)))
                 model.add(Activation('relu'))
                 model.add(MaxPooling2D(pool_size=(2, 2)))
@@ -43,22 +43,22 @@ class TrainModel:
 
 
                 #callbacks
-                # callback_list = [
-                #         keras.callbacks.EarlyStopping(
-                #                 monitor='acc',
-                #                 patience=1,
-                #         ),
-                #         keras.callbacks.ModelCheckpoint(
-                #                 filepath='model/first_try.h5',
-                #                 monitor='val_loss',
-                #                 save_best_only=True
-                #         ),
-                #         keras.callbacks.ReduceLROnPlateau(
-                #                 monitor='val_loss',
-                #                 factor=0.1,
-                #                 patience=1
-                #         )
-                # ]
+                callback_list = [
+                         EarlyStopping(
+                                 monitor='acc',
+                                 patience=1,
+                         ),
+                         ModelCheckpoint(
+                                 filepath='model/first_try.h5',
+                                 monitor='val_loss',
+                                 save_best_only=True
+                         ),
+                         ReduceLROnPlateau(
+                                 monitor='val_loss',
+                                 factor=0.1,
+                                 patience=1
+                         )
+                ]
 
 
                 model.compile(loss='categorical_crossentropy',
@@ -83,26 +83,47 @@ class TrainModel:
                         batch_size=batch_size,
                         class_mode='categorical')
 
-                callbacks = [
-                        TensorBoardWrapper(
-                                validation_generator,
-                                nb_steps=5,
-                                log_dir='my_log_dir/{}'.format(time()),
-                                histogram_freq=1,
-                                batch_size=32,
-                                write_graph=False,
-                                write_grads=True)]
+                #callbacks = [
+                #        TensorBoardWrapper(
+                #                validation_generator,
+                #                nb_steps=5,
+                #                log_dir='my_log_dir/{}'.format(time()),
+                #                histogram_freq=1,
+                #                batch_size=32,
+                #                write_graph=True,
+                #                write_grads=True)]
+
 
                 try:
-                        model.fit_generator(
+                        history = model.fit_generator(
                                 train_generator,
                                 #callbacks=callback_list,
-                                callbacks=callbacks,
+                                callbacks=callback_list,
                                 epochs=1,
-                                steps_per_epoch= 6000/batch_size,
+                                steps_per_epoch=5, #6000/batch_size,
                                 validation_data=validation_generator,
-                                validation_steps=2466/batch_size
+                                validation_steps=1, #2466/batch_size
+                                verbose = 1
                                 )
+
+                        print(history.history)
+                        # summarize history for accuracy
+                        plt.plot(history.history['acc'])
+                        plt.plot(history.history['val_acc'])
+                        plt.title('model accuracy')
+                        plt.ylabel('accuracy')
+                        plt.xlabel('epoch')
+                        plt.legend(['train', 'test'], loc='upper left')
+                        plt.show()
+                        # summarize history for loss
+                        plt.plot(history.history['loss'])
+                        plt.plot(history.history['val_loss'])
+                        plt.title('model loss')
+                        plt.ylabel('loss')
+                        plt.xlabel('epoch')
+                        plt.legend(['train', 'test'], loc='upper left')
+                        plt.show()
+
                         model.save_weights('model/first_try.h5')
                 except KeyboardInterrupt:
                         model.save_weights('model/first_try.h5')
