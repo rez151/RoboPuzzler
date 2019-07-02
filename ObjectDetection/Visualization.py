4de1a3f331a3ae1145ac93be2cc0c3e75ac42216
Daniel <daniel.ertl1988@googlemail.com>
2019-07-02 13:10:41 +0200

    adding visualheat to main and extend heatmap to all layers

--- ObjectDetection/Visualization.py
+++ ObjectDetection/Visualization.py
@@ -10,7 +10,7 @@ import cv2
 class Visualization:
     def visualization(self, img):
         model = load_model('model/using/model.h5')
-        model.summary()
+        #model.summary()
         layer_outputs = [layer.output for layer in model.layers[:7]]
         activation_model = models.Model(inputs=model.input,
                                         outputs=layer_outputs)
@@ -40,35 +40,43 @@ class Visualization:
             )
             plt.show(bbox_inches='tight', pad_inches=0)
 
-    def visualheat(self, img):
+    def visualheat(self, img, id):
         model = load_model('model/using/model.h5')
-        model.summary()
+        #model.summary()
         image = self.preprocessing(img)
         preds = model.predict(image)
+        layer_names = ['conv2d_1', 'conv2d_2', 'conv2d_3']
+        layer_sizes = [32, 32, 64]
+        j = 0
+        orig_img = cv2.imread(img)
+        superimposed_img = orig_img
+        for name in layer_names:
+            output = model.output[:, np.argmax(preds[0])]
+            last_conv_layer = model.get_layer(name)
+            grads = K.gradients(output,last_conv_layer.output)[0]
+            pooled_grads = K.mean(grads, axis=(0, 1, 2))
+            iterate = K.function([model.input],
+                                 [pooled_grads, last_conv_layer.output[0]])
+            pooled_grads_value, conv_layer_output_value = iterate([image])
+            for i in range(layer_sizes[j]): conv_layer_output_value[:, :, i] *= pooled_grads_value[i]
+            heatmap = np.mean(conv_layer_output_value, axis=-1)
+            heatmap = np.maximum(heatmap, 0)
+            heatmap /= np.max(heatmap)
+            plt.figure()
+            plt.imshow(heatmap)
+            plt.axis('off')
+            plt.show()
+            heatmap = cv2.resize(heatmap, (orig_img.shape[1], orig_img.shape[0]))
+            heatmap = np.uint8(255 * heatmap)
+            heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+            #superimposed_img = heatmap * 0.4 + image
+            superimposed_img = heatmap * 0.5 + orig_img
+            j += 1
+            cv2.imwrite('log_img/Visualization/tmp.png', superimposed_img)
 
-        african_elephant_output = model.output[:, np.argmax(preds[0])]
-        last_conv_layer = model.get_layer('conv2d_3')
-        grads = K.gradients(african_elephant_output,last_conv_layer.output)[0]
-        pooled_grads = K.mean(grads, axis=(0, 1, 2))
-        iterate = K.function([model.input],
-                             [pooled_grads, last_conv_layer.output[0]])
-        pooled_grads_value, conv_layer_output_value = iterate([image])
-        for i in range(64): conv_layer_output_value[:, :, i] *= pooled_grads_value[i]
-        heatmap = np.mean(conv_layer_output_value, axis=-1)
-        heatmap = np.maximum(heatmap, 0)
-        heatmap /= np.max(heatmap)
-        plt.figure()
-        plt.imshow(heatmap)
-        plt.axis('off')
-        plt.show()
-        source_img = cv2.imread(img)
-        gray_img = cv2.cvtColor(source_img, cv2.COLOR_BGR2GRAY)
-        heatmap = cv2.resize(heatmap, (gray_img.shape[1], gray_img.shape[0]))
-        heatmap = np.uint8(224 * heatmap)
-        heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
-        superimposed_img = heatmap * 0.4 + cv2.cvtColor(gray_img, cv2.COLOR_GRAY2RGB)
-        cv2.imwrite('log_img/Visualization/cam.jpg',
-                    superimposed_img)
+        cv2.imwrite('log_img/Visualization/{}.jpg'.format(
+            id),
+            superimposed_img)
 
 
 
@@ -82,5 +90,5 @@ class Visualization:
 
 
 if __name__ == '__main__':
-    img_path = 'Images/test/Lowe/2.jpg'
-    Visualization().visualheat(img_path)
+    img_path = 'log_img/Visualization/tmp.png'
+    Visualization().visualheat(img_path, 9)
